2ca0cdeb040b1e29846c6a09d680c7f6befde81d
Kasim8055 <139548783+Kasim-Tech@users.noreply.github.com>
2024-06-24 12:28:25 +0530

    Login&signup done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { useState } from "react";
 import {Link} from 'react-router-dom'
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("Home");
 
   return (
@@ -45,7 +45,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="cart" />
             <div className="dot"></div>
           </div>
-          <button className="SignBTN">Sign in</button>
+          <button onClick={()=>setShowLogin(true)} className="SignBTN">Sign in</button>
         </div>
       </div>
     </>
